49a6db8bcd9ad370c65f0d194437cf36e710c3eb
Dip <yarik.d.kachanov@gmail.com>
2024-09-22 22:39:19 +0300

    feat: css for navigations and footer

--- eco-sounds/script.js
+++ eco-sounds/script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     createAudio();
-    // myAudioElement();
     imgClic();
+    logoClic();
 });
 
 function createAudio() {
@@ -14,47 +14,79 @@ function startAudio() {
     audioObj.play();
 }
 
-// myAudioElement.addEventListener("canplaythrough", (event) => {
-//     audioObj = new Audio();
-//     audioObj.play();
-//     /* аудио может быть воспроизведено; проиграть, если позволяют разрешения */
-//   });
-
 function imgClic() {
     let logo = document.getElementById('logo');
     logo.onclick = () => logoClic();
+
     let solovey = document.getElementById('solovey');
     solovey.onclick = () => soloveyClic();
+
     let drozd = document.getElementById('drozd');
     drozd.onclick = () => drozdClic();
+
     let malinovka = document.getElementById('malinovka');
     malinovka.onclick = () => malinovkaClic();
+
     let jayvoronok = document.getElementById('jayvoronok');
     jayvoronok.onclick = () => jayvoronokClic();
+
     let slavka = document.getElementById('slavka');
-    slavka.onclick = () => slavkalic();
+    slavka.onclick = () => slavkaClic();
 }
 
 function logoClic() {
     document.getElementById('fon-img').style.backgroundImage = "url(./assets/img/forest.jpg)";
+
+    nonClicStyle();
+
+    document.getElementById('logo').style.filter = "invert(26%) sepia(15%) saturate(638%) hue-rotate(7deg) brightness(92%) contrast(87%)";
 }
 
 function soloveyClic() {
     document.getElementById('fon-img').style.backgroundImage = "url(./assets/img/solovey.jpg)";
+
+    nonClicStyle();
+
+    document.getElementById('solovey').style.filter = "invert(26%) sepia(15%) saturate(638%) hue-rotate(7deg) brightness(92%) contrast(87%)";
 }
 
 function drozdClic() {
     document.getElementById('fon-img').style.backgroundImage = "url(./assets/img/drozd.jpg)";
+    
+    nonClicStyle();
+
+    document.getElementById('drozd').style.filter = "invert(26%) sepia(15%) saturate(638%) hue-rotate(7deg) brightness(92%) contrast(87%)";
 }
 
 function malinovkaClic() {
     document.getElementById('fon-img').style.backgroundImage = "url(./assets/img/zarynka.jpg)";
+    
+    nonClicStyle();
+
+    document.getElementById('malinovka').style.filter = "invert(26%) sepia(15%) saturate(638%) hue-rotate(7deg) brightness(92%) contrast(87%)";
 }
 
 function jayvoronokClic() {
     document.getElementById('fon-img').style.backgroundImage = "url(./assets/img/javoronok.jpg)";
+    
+    nonClicStyle();
+
+    document.getElementById('jayvoronok').style.filter = "invert(26%) sepia(15%) saturate(638%) hue-rotate(7deg) brightness(92%) contrast(87%)";
 }
 
-function slavkalic() {
+function slavkaClic() {
     document.getElementById('fon-img').style.backgroundImage = "url(./assets/img/slavka.jpg)";
+
+    nonClicStyle();
+
+    document.getElementById('slavka').style.filter = "invert(26%) sepia(15%) saturate(638%) hue-rotate(7deg) brightness(92%) contrast(87%)";
+}
+
+function nonClicStyle() {
+    document.getElementById('logo').style.filter = "";
+    document.getElementById('solovey').style.filter = "";
+    document.getElementById('drozd').style.filter = "";
+    document.getElementById('malinovka').style.filter = "";
+    document.getElementById('jayvoronok').style.filter = "";
+    document.getElementById('slavka').style.filter = "";
 }
